f743d9c87d3af2288a16b240bed7e6995d4f2a0d
DedPerded <lightman76@yandex.ru>
2022-09-24 21:43:07 +0300

    CombineResult and ExecutePipeline left

--- 02_async/99_hw/signer/signer.go
+++ 02_async/99_hw/signer/signer.go
@@ -2,39 +2,51 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 )
 
-func ExecutePipeline() {
-
-}
+var md5Mutex = &sync.Mutex{}
 
 func takeMd5Hash(data string, out chan string) {
+	md5Mutex.Lock()
 	out <- DataSignerMd5(data)
+	md5Mutex.Unlock()
 }
 
 func takeCrc32Hash(data string, out chan string) {
 	out <- DataSignerCrc32(data)
 }
 
-func SingleHash(in, out chan string) {
-	data := <-in
-	go takeMd5Hash(data, out)
-	go takeCrc32Hash(data, out)
-	go takeCrc32Hash(<-out, out)
-	out <- <-out + "~" + <-out
+func SingleHash(in, out chan interface{}) {
+	dataRaw := <-in
+	data, ok := dataRaw.(string)
+	if !ok {
+		fmt.Println("cant convert result data to string")
+	}
+	outString := make(chan string, 1)
+	go takeMd5Hash(data, outString)
+	go takeCrc32Hash(data, outString)
+	go takeCrc32Hash(<-outString, outString)
+
+	out <- <-outString + "~" + <-outString
 }
 
-func MultiHash(in, out chan string) {
-	data := <-in
-	// for i := 0; i < 6; i++ {
-	// 	go takeCrc32Hash(strconv.Itoa(i)+data, out)
-	// }
-	ticker := time.NewTicker(10 * time.Millisecond)
+func MultiHash(in, out chan interface{}) {
+	dataRaw := <-in
+	data, ok := dataRaw.(string)
+	if !ok {
+		fmt.Println("cant convert result data to string")
+	}
+	outString := make(chan string, 6)
+
+	ticker := time.NewTicker(5 * time.Millisecond)
 	i := 0
 	for tickTime := range ticker.C {
-		go takeCrc32Hash(strconv.Itoa(i)+data, out)
+		go takeCrc32Hash(strconv.Itoa(i)+data, outString)
 		if i == 5 {
 			ticker.Stop()
 			break
@@ -45,32 +57,59 @@ func MultiHash(in, out chan string) {
 
 	var answer string
 	for i := 0; i < 6; i++ {
-		msg := <-out
-		fmt.Println(i, msg)
+		msg := <-outString
+		// fmt.Println(i, msg)
 		answer += msg
 	}
 	fmt.Println("MultiHash result", answer)
 	out <- answer
 }
 
-func CombineResult(in, out chan string) {
+func CombineResult(in, out chan interface{}) {
+	var allData []string
+	// for rawData := range in {
+	// 	data, ok := rawData.(string)
+	// 	if !ok {
+	// 		fmt.Println("cant convert result data to string")
+	// 	}
+	// 	allData = append(allData, data)
+	// }
+	rawData := <-in
+	data := rawData.(string)
+	allData = append(allData, data)
+	rawData = <-in
+	data = rawData.(string)
+	allData = append(allData, data)
+
+	sort.Strings(allData)
+	out <- strings.Join(allData, "_")
 
 }
 
+// func ExecutePipeline(funcs ...job) {
+// }
+
 func main() {
 	start := time.Now()
-	in := make(chan string, 6)
-	out := make(chan string, 6)
+	// ExecutePipeline()
+	in := make(chan interface{}, 6)
+	out := make(chan interface{}, 6)
 	in <- "0"
-	SingleHash(in, out)
-	// SingleHashOut := <-out
-	// in <- SingleHashOut
+	in <- "1"
+
+	go SingleHash(in, out)
+	go SingleHash(in, out)
+	// go MultiHash(out, in)
 	MultiHash(out, in)
-	// CombineResult(in, out)
+	MultiHash(out, in)
+	// close(out)
+	CombineResult(in, out)
+
 	end := time.Since(start)
 	fmt.Println(end)
-
-	// fmt.Println(SingleHashOut)
+	// time.Sleep(2 * time.Second)
+	fmt.Println(<-out)
+	// fmt.Println(<-out)
 
 	// for msg := range in {
 	// 	fmt.Println(msg)
