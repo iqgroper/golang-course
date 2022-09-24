beb8da4b99803479809530797090bdb7ca3b41c5
DedPerded <lightman76@yandex.ru>
2022-09-24 18:08:11 +0300

    started, linear SingleHash s written

--- 02_async/99_hw/signer/signer.go
+++ 02_async/99_hw/signer/signer.go
@@ -1,3 +1,33 @@
 package main
 
-// сюда писать код
+import "fmt"
+
+func ExecutePipeline() {
+
+}
+
+func SingleHash(in, out chan string) {
+	data := <-in
+	md5 := DataSignerMd5(data)
+	crc32FromMd5 := DataSignerCrc32(md5)
+	crc32FromData := DataSignerCrc32(data)
+	out <- crc32FromData + "~" + crc32FromMd5
+}
+
+func MultiHash(in, out chan string) {
+
+}
+
+func CombineResult() {
+
+}
+
+func main() {
+	in := make(chan string, 1)
+	out := make(chan string, 1)
+	in <- "0"
+	SingleHash(in, out)
+
+	fmt.Println(<-out)
+
+}
