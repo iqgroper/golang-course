44d9ec7b3cdf05587e4cf1da4707090fa1ef9f89
DedPerded <lightman76@yandex.ru>
2022-09-24 20:40:20 +0300

    Single and Multi ready, takes 2 sec

--- 02_async/99_hw/signer/signer.go
+++ 02_async/99_hw/signer/signer.go
@@ -1,33 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
 func ExecutePipeline() {
 
 }
 
+func takeMd5Hash(data string, out chan string) {
+	out <- DataSignerMd5(data)
+}
+
+func takeCrc32Hash(data string, out chan string) {
+	out <- DataSignerCrc32(data)
+}
+
 func SingleHash(in, out chan string) {
 	data := <-in
-	md5 := DataSignerMd5(data)
-	crc32FromMd5 := DataSignerCrc32(md5)
-	crc32FromData := DataSignerCrc32(data)
-	out <- crc32FromData + "~" + crc32FromMd5
+	go takeMd5Hash(data, out)
+	go takeCrc32Hash(data, out)
+	go takeCrc32Hash(<-out, out)
+	out <- <-out + "~" + <-out
 }
 
 func MultiHash(in, out chan string) {
+	data := <-in
+	// for i := 0; i < 6; i++ {
+	// 	go takeCrc32Hash(strconv.Itoa(i)+data, out)
+	// }
+	ticker := time.NewTicker(10 * time.Millisecond)
+	i := 0
+	for tickTime := range ticker.C {
+		go takeCrc32Hash(strconv.Itoa(i)+data, out)
+		if i == 5 {
+			ticker.Stop()
+			break
+			fmt.Println(tickTime)
+		}
+		i++
+	}
 
+	var answer string
+	for i := 0; i < 6; i++ {
+		msg := <-out
+		fmt.Println(i, msg)
+		answer += msg
+	}
+	fmt.Println("MultiHash result", answer)
+	out <- answer
 }
 
-func CombineResult() {
+func CombineResult(in, out chan string) {
 
 }
 
 func main() {
-	in := make(chan string, 1)
-	out := make(chan string, 1)
+	start := time.Now()
+	in := make(chan string, 6)
+	out := make(chan string, 6)
 	in <- "0"
 	SingleHash(in, out)
+	// SingleHashOut := <-out
+	// in <- SingleHashOut
+	MultiHash(out, in)
+	// CombineResult(in, out)
+	end := time.Since(start)
+	fmt.Println(end)
 
-	fmt.Println(<-out)
+	// fmt.Println(SingleHashOut)
 
+	// for msg := range in {
+	// 	fmt.Println(msg)
+	// }
 }
