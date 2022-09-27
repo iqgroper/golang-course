b057e1ae6ccb849601450fd2706ba774668e5abf
DedPerded <lightman76@yandex.ru>
2022-09-27 22:18:15 +0300

    wroks=)

--- 02_async/99_hw/signer/signer.go
+++ 02_async/99_hw/signer/signer.go
@@ -12,19 +12,13 @@ import (
 
 var md5Mutex = &sync.Mutex{}
 
-// var wg = &sync.WaitGroup{}
-
-func takeMd5Hash(data string, out chan string, waiter *sync.WaitGroup) {
+func takeMd5Hash(data string, out chan string) {
 	md5Mutex.Lock()
 	out <- DataSignerMd5(data)
-	waiter.Done()
 	md5Mutex.Unlock()
 }
 
-func takeCrc32Hash(data string, out chan string, waiter *sync.WaitGroup) {
-	if waiter != nil {
-		defer waiter.Done()
-	}
+func takeCrc32Hash(data string, out chan string) {
 	out <- DataSignerCrc32(data)
 }
 
@@ -39,14 +33,9 @@ func InnerSingleHash(in, out chan interface{}, waiter *sync.WaitGroup) {
 	data := strconv.Itoa(checkedData)
 	outString := make(chan string, 1)
 
-	innerWaiter := &sync.WaitGroup{}
-	innerWaiter.Add(3)
-	go takeMd5Hash(data, outString, innerWaiter)
-	go takeCrc32Hash(data, outString, innerWaiter)
-	go takeCrc32Hash(<-outString, outString, innerWaiter)
-
-	// innerWaiter.Wait()
-	// out <- <-outString + "~" + <-outString
+	go takeMd5Hash(data, outString)
+	go takeCrc32Hash(data, outString)
+	go takeCrc32Hash(<-outString, outString)
 
 	result := <-outString + "~" + <-outString
 	out <- result
@@ -80,7 +69,7 @@ func InnerMultiHash(in, out chan interface{}, waiter *sync.WaitGroup) {
 	ticker := time.NewTicker(5 * time.Millisecond)
 	i := 0
 	for tickTime := range ticker.C {
-		go takeCrc32Hash(strconv.Itoa(i)+data, outString, nil)
+		go takeCrc32Hash(strconv.Itoa(i)+data, outString)
 		if i == 5 {
 			ticker.Stop()
 			break
@@ -151,36 +140,9 @@ func ExecutePipeline(jobs ...job) {
 	for i := 0; i < len(jobs); i++ {
 		waitJobs.Add(1)
 		go callingJob(jobs[i], channels[i], channels[i+1], true, waitJobs)
-		// time.Sleep(time.Second)
 	}
 	waitJobs.Wait()
 
-	// jobs[0](channels[0], channels[1])
-	// // close(channels[1])
-	// go jobs[1](channels[1], channels[2])
-	// // close(channels[2])
-	// jobs[2](channels[2], nil)
-	// time.Sleep(time.Millisecond)
-
-	// go jobs[1](out, in)
-	// jobs[0](in, out)
-
-	// jobs[0](in, out)
-
-	// inputNumber := len(out)
-	// for i := 0; i < inputNumber; i++ {
-	// 	go jobs[1](out, in)
-	// }
-
-	// for i := 0; i < inputNumber; i++ {
-	// 	wg.Add(1)
-	// 	go jobs[2](in, out)
-	// }
-	// wg.Wait()
-
-	// close(out)
-	// jobs[3](out, in)
-	// jobs[4](in, out)
 }
 
 func main() {
