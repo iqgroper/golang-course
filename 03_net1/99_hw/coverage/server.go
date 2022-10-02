b43dfc6ba1fb7f076d4393368bd18e39989aa754
DedPerded <lightman76@yandex.ru>
2022-10-02 17:50:07 +0300

    server writes answers to a client in json, transforming response accordingly and tests left

--- 03_net1/99_hw/coverage/server.go
+++ 03_net1/99_hw/coverage/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
@@ -33,13 +34,30 @@ type Rows struct {
 	List    []Row  `xml:"row"`
 }
 
+type Query struct {
+	Name  string `json:"name"`
+	About string `json:"about"`
+}
+
+type Queries struct {
+	QueryList []Query
+}
+
+type OrderField struct {
+	Id   int    `json:"id"`
+	Age  int    `json:"age"`
+	Name string `json:"name"`
+}
+
+type Orders struct {
+	OrderList []OrderField
+}
+
 type Handler struct {
 	Name string
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Fprintln(w, "Name:", h.Name, "URL:", r.URL.String())
 	SearchServer(w, r)
 }
 
@@ -49,21 +67,70 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	rows := new(Rows)
+	rows := Rows{}
 	err = xml.Unmarshal([]byte(data), &rows)
 	if err != nil {
 		panic(err)
 	}
 
 	query := r.FormValue("query")
+	queryJSON := Queries{}
 	if query != "" {
+		err = json.Unmarshal([]byte(query), &queryJSON)
+		if err != nil {
+			panic(err)
+		}
+
 		fmt.Println("query is", query)
 	}
-
 	orderField := r.FormValue("order_field")
+	orderFieldJSON := Orders{}
 	if orderField != "" {
+		err = json.Unmarshal([]byte(orderField), &orderFieldJSON)
+		if err != nil {
+			panic(err)
+		}
+
 		fmt.Println("orderField is", orderField)
 	}
+	orderBy := r.FormValue("order_by")
+	if orderBy != "" {
+		fmt.Println("order_by is", orderBy)
+	}
+	limit := r.FormValue("limit")
+	if limit != "" {
+		fmt.Println("limit is", limit)
+	}
+	offset := r.FormValue("offset")
+	if offset != "" {
+		fmt.Println("offset is", offset)
+	}
+
+	responseBody := make([]User, len(orderFieldJSON.OrderList))
+
+	for i, order := range orderFieldJSON.OrderList {
+		responseBody[i] = User{
+			ID:     rows.List[order.Id].Id,
+			Name:   rows.List[order.Id].FirstName + rows.List[order.Id].LastName,
+			Age:    rows.List[order.Id].Age,
+			About:  rows.List[order.Id].About,
+			Gender: rows.List[order.Id].Gender,
+		}
+	}
+
+	// fmt.Println("response:")
+	// for i, resp := range responseBody {
+	// 	fmt.Println(i, resp)
+	// }
+
+	body, err := json.Marshal(responseBody)
+	if err != nil {
+		panic(err)
+	}
+
+	// fmt.Println(string(body))
+
+	w.Write(body)
 }
 
 func clientHit() {
@@ -77,8 +144,8 @@ func clientHit() {
 	request := SearchRequest{
 		Limit:      25,
 		Offset:     0,
-		Query:      "{\"name\":\"Ivan\"}",
-		OrderField: "{\"id\":0}",
+		Query:      "{\"querylist\":[{\"name\":\"Ivan\"}, {\"name\":\"Ivan2\"}]}",
+		OrderField: "{\"orderlist\":[{\"id\":1},{\"id\":2}]}",
 		OrderBy:    0,
 	}
 	resp, err := client.FindUsers(request)
@@ -87,7 +154,7 @@ func clientHit() {
 		return
 	}
 
-	fmt.Println(resp)
+	fmt.Println("resp", resp)
 }
 
 func main() {
