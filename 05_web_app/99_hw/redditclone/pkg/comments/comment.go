9d0680508a0b8f5829d05641421dd596dd00e1f0
DedPerded <lightman76@yandex.ru>
2022-10-31 17:01:35 +0300

    deleted redundant comments

--- 05_web_app/99_hw/redditclone/pkg/comments/comment.go
+++ 05_web_app/99_hw/redditclone/pkg/comments/comment.go
@@ -11,7 +11,6 @@ type Comment struct {
 }
 
 type CommentRepo interface {
-	// GetByID(id uint) (*Comment, error)
 	Add(post_id uint, body string, user *user.User) (*Comment, error)
 	GetAll(post_id uint) ([]*Comment, error)
 	Delete(post_id, comment_id uint) (bool, error)
