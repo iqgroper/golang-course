fee97bf210fc04236d539d9fbf490b05fe068993
DedPerded <lightman76@yandex.ru>
2022-11-26 20:41:45 +0300

    created Biz service

--- 08_microservices/99_hw/microservice/service.go
+++ 08_microservices/99_hw/microservice/service.go
@@ -1,5 +1,53 @@
 package main
 
-// тут вы пишете код
-// обращаю ваше внимание - в этом задании запрещены глобальные переменные
-// если хочется, то для красоты можно разнести логику по разным файликам
+import (
+	context "context"
+	"fmt"
+	"log"
+	"net"
+
+	grpc "google.golang.org/grpc"
+)
+
+func StartMyMicroservice(ctx context.Context, listenAddr, ACLData string) error {
+	lis, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Fatalln("cant listet port", err)
+	}
+
+	server := grpc.NewServer(
+	// grpc.UnaryInterceptor(authInterceptor),
+	// grpc.InTapHandle(rateLimiter),
+	)
+
+	// RegisterAdminServer(server, )
+	RegisterBizServer(server, NewBiz())
+
+	fmt.Println("starting server at", listenAddr)
+	server.Serve(lis)
+	return nil
+}
+
+type Biz struct {
+	UnimplementedBizServer
+	ServiceName string
+}
+
+func NewBiz() *Biz {
+	return &Biz{ServiceName: "Some buisness logic"}
+}
+
+func (biz *Biz) Check(context.Context, *Nothing) (*Nothing, error) {
+	fmt.Println("Biz.Check logic")
+	return nil, nil
+}
+
+func (biz *Biz) Add(context.Context, *Nothing) (*Nothing, error) {
+	fmt.Println("Biz.Add logic")
+	return nil, nil
+}
+
+func (biz *Biz) Test(context.Context, *Nothing) (*Nothing, error) {
+	fmt.Println("Biz.Test logic")
+	return nil, nil
+}
